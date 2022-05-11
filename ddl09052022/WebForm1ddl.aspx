addcc23934c37e030672d082ecc0f2e53e33ac2a
suman <suman@SUMAN>
2022-05-11 20:36:24 +0530

    Add project files.

---
+++ ddl09052022/WebForm1ddl.aspx
@@ -0,0 +1,93 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1ddl.aspx.cs" Inherits="ddl09052022.WebForm1ddl" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table >
+                <tr>
+                    <td> Name: </td>
+                     <td> <asp:TextBox id="txtName" runat="server"> </asp:TextBox> </td>
+                </tr>
+
+                <tr>
+                    <td> Age: </td>
+                     <td> <asp:TextBox id="TxtAge" runat="server"> </asp:TextBox> </td>
+                </tr>
+
+                 <tr>
+                    <td> Blood Group: </td>
+                     <td> <asp:RadioButtonList ID="rblbg" RepeatColumns="6" runat ="server"></asp:RadioButtonList> </td>
+                </tr>
+
+                 <tr>
+                    <td> Country: </td>
+                     <td> <asp:DropDownList id="DdlCountry" runat="server" ></asp:DropDownList> </td>
+                </tr>
+
+                 <tr>
+                    <td> </td>
+
+                    <td> <asp:Button id="btnsave" Text ="Save" OnClick="btnsave_Click" runat="server"> </asp:Button> </td>
+                </tr>
+
+                   <tr>  
+                    <td> </td>
+                   <td> <asp:GridView id="grdview" runat="server" AutoGenerateColumns="false" OnRowCommand ="grdview_RowCommand">
+                        <Columns> 
+                        <asp:TemplateField HeaderText="Userid">
+                        <ItemTemplate>
+                         <%#Eval("Userid") %>
+                        </ItemTemplate>
+                        </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="UserName">
+                        <ItemTemplate>
+                         <%#Eval("UserName") %>
+                        </ItemTemplate>
+                        </asp:TemplateField>
+
+                            <asp:TemplateField HeaderText="UserAge">
+                        <ItemTemplate>
+                         <%#Eval("UserAge") %>
+                        </ItemTemplate>
+                        </asp:TemplateField>
+
+
+                        <asp:TemplateField HeaderText="User Blood Group">
+                        <ItemTemplate>
+                         <%#Eval("bgname") %>
+                        </ItemTemplate>
+                        </asp:TemplateField>
+
+                        <asp:TemplateField HeaderText="User Country">
+                        <ItemTemplate>
+                         <%#Eval("Cname") %>
+                        </ItemTemplate>
+                        </asp:TemplateField>
+
+             <asp:TemplateField >
+             <ItemTemplate>
+             <asp:Button id="BtnDelete" runat="server" Text="Delete" CommandArgument='<%#Eval("Userid") %>'  CommandName="A" />
+             </ItemTemplate>
+             </asp:TemplateField>
+
+             <asp:TemplateField>
+             <ItemTemplate>
+             <asp:Button id="BtnEdit" runat="server" Text="Edit" CommandArgument='<%#Eval("Userid") %>'  CommandName="B" />
+             </ItemTemplate>
+             </asp:TemplateField>
+             </Columns> 
+
+             </asp:GridView> </td>
+             </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ddl09052022/WebForm1ddl.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+namespace ddl09052022
+{
+    public partial class WebForm1ddl : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source = SUMAN\\SQLEXPRESS; Initial catalog = ddl09052022; integrated Security = true");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          if(!IsPostBack)  
+            {
+                BindGrid();
+                BindBloodGroup();
+                BindCountry();
+            }
+        }
+        void BindBloodGroup()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from tblBloodGroup", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            rblbg.DataValueField = "bgid";
+            rblbg.DataTextField = "bgname";
+            rblbg.DataSource = dt;
+            rblbg.DataBind();
+
+            //grdview.DataSource = dt;
+            //grdview.DataBind();
+        }
+
+        void BindGrid()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from tblusers join tblBloodGroup on UserBg=bgid join tblddlCountry on UserCountry=Cid ", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+                  
+            grdview.DataSource = dt;
+            grdview.DataBind();
+
+        }
+
+        void BindCountry()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from tblddlCountry", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            DdlCountry.DataValueField = "Cid";
+            DdlCountry.DataTextField = "Cname";
+            DdlCountry.DataSource = dt;
+            DdlCountry.DataBind();
+            DdlCountry.Items.Insert(0, new ListItem("--select--", "0"));
+        }
+
+        public void Clear()
+        {
+            txtName.Text = "";
+            TxtAge.Text = "";
+            rblbg.ClearSelection();
+            DdlCountry.SelectedValue = "0";
+            btnsave.Text = "Save";
+
+        }
+
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            if (btnsave.Text == "Save")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Insert into tblusers (UserName,UserAge,UserBg, UserCountry)values ('" + txtName.Text + "', '" + TxtAge.Text + "','" + rblbg.SelectedValue + "','" + DdlCountry.SelectedValue + "')", con);
+                cmd.ExecuteNonQuery(); 
+                con.Close();
+                BindGrid();
+                Clear();
+            }
+            else if (btnsave.Text == "Update")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Update tblusers set UserName='" + txtName.Text + "', UserAge= '" + TxtAge.Text + "',UserBg='" + rblbg.SelectedValue + "',UserCountry='" + DdlCountry.SelectedValue + "'where Userid= '" + ViewState["id"] +"'", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindGrid();
+                Clear();            }
+
+        }
+
+        protected void grdview_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "A")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("delete from tblusers where Userid='" + e.CommandArgument +"'", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindGrid();
+            }
+
+            else if (e.CommandName == "B")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from tblusers where Userid='" + e.CommandArgument+ "'", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                con.Close();
+               
+                txtName.Text = dt.Rows[0]["UserName"].ToString();
+                TxtAge.Text = dt.Rows[0]["UserAge"].ToString();
+                rblbg.SelectedValue = dt.Rows[0]["UserBg"].ToString();
+                DdlCountry.SelectedValue = dt.Rows[0]["UserCountry"].ToString();
+                btnsave.Text = "Update";
+                ViewState["id"] = e.CommandArgument;
+            }
+        }
+    }
+}
